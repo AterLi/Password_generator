e6e5d8722eec3df3fda9e04b5e88ec8a22235144
AterLi <batrinu.alin825@gmail.com>
2024-08-15 17:13:30 +0300

    Second commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+l=random.sample(letters, nr_letters)
+s=random.sample(numbers, nr_symbols)
+n=random.sample(symbols, nr_symbols)
+
+merged=[l,s,n]
+shuffeled_pass = random.shuffle(merged)
+print(shuffeled_pass)
+resultJoined = ''
+
+for i in merged:
+    resultJoined += ''.join(i)+ ''
+
+# print("After shuffle:\n",resultJoined)
+
+succes= random.sample(resultJoined, len(resultJoined))
+print("Your pass:")
+
+for i in succes:
+    print(*i, end='')
