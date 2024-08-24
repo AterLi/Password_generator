e2725e6744d2cb9e81c77502344658f2f92eb4d5
AterLi <132461103+AterLi@users.noreply.github.com>
2024-08-24 13:08:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 import random
+from random import shuffle
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-l=random.sample(letters, nr_letters)
-s=random.sample(numbers, nr_symbols)
-n=random.sample(symbols, nr_symbols)
+generated_pass = []
 
-merged=[l,s,n]
-shuffeled_pass = random.shuffle(merged)
-print(shuffeled_pass)
-resultJoined = ''
+num_letters = int(input("How much letters for your password: "))
+for _ in range(num_letters):
+    rand_let = random.choice(letters)
+    generated_pass.append(rand_let)
 
-for i in merged:
-    resultJoined += ''.join(i)+ ''
+num_numbers = int(input("How much numbers for your password: "))
+for i in range(num_numbers):
+    rand_num = random.choice(numbers)
+    generated_pass.append(rand_num)
 
-# print("After shuffle:\n",resultJoined)
+num_symbols = int(input("How much symbols for your password: "))
+for _ in range(num_symbols):
+    rand_symb = random.choice(symbols)
+    generated_pass.append(rand_symb)
 
-succes= random.sample(resultJoined, len(resultJoined))
-print("Your pass:")
 
-for i in succes:
-    print(*i, end='')
+print(f"Initial pass: {''.join(generated_pass)}")
+shuffle(generated_pass)
+print(f"After using shuffle function: {''.join(generated_pass)}")
+
+
+# for i in generated_pass:
+#     print(i, end="")
+
